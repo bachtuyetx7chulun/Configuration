ac69ece2f7345be9da936bdc7fb8e9b1eaef22fb
bachtuyetx7chulun <contact.truongtrunghieu@gmail.com>
2021-07-17 18:26:14 +0700

    ts config with react

---
+++ react.eslint.ts
@@ -0,0 +1,1 @@
+yarn add @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-airbnb eslint-config-prettier eslint-import-resolver-typescript eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-prettier eslint-plugin-react eslint-plugin-react-hooks prettier -D
